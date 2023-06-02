aa2aac0d4aa346fa1c47ecd4a4343066adf12808
Тавабилов Денис Ринатович <d.tavabilov@prosoftsystems.ru>
2023-06-02 09:03:13 +0500

    Добавление devserver

--- config/build/buildWebpackConfig.ts
+++ config/build/buildWebpackConfig.ts
@@ -5,24 +5,27 @@ import path from "path";
 import { buildLoaders } from "./buildLoaders";
 import { buildResolvers } from "./buildResolvers";
 import { buildPlugins } from "./buildPlugins";
+import { buildDevServer } from "./buildDevServer";
 
 export function buildWebpackConfig(
   options: BuildOptions
 ): webpack.Configuration {
-  const { mode, paths } = options;
+  const { mode, paths, isDev } = options;
 
   return {
     mode,
     entry: paths.entry,
-    module: {
-      rules: buildLoaders(),
-    },
-    resolve: buildResolvers(),
     output: {
       filename: "[name].[contenthash].js",
       path: paths.build,
       clean: true,
     },
     plugins: buildPlugins(options),
+    module: {
+      rules: buildLoaders(),
+    },
+    resolve: buildResolvers(),
+    devtool: isDev ? "inline-source-map" : undefined,
+    devServer: isDev ? buildDevServer(options) : undefined,
   };
 }
--- src/test.ts
+++ src/test.ts
@@ -1,4 +1,4 @@
 export function someFn(value: number): string {
-  console.log("hello world");
+  console.log("hello world!!");
   return ''
 }

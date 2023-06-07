fd79a5ca349fa6696f99f097f1c47e3ff0983dd6
Тавабилов Денис Ринатович <d.tavabilov@prosoftsystems.ru>
2023-06-07 08:38:13 +0500

    Добавление стилей, темный светлый режим, хуки

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,20 @@
-import "./index.scss";
+import "./styles/index.scss";
 import { Route, Routes } from "react-router-dom";
-import AboutPage from "./pages/AboutPage/AboutPage";
-import MainPage from "./pages/MainPage/MainPage";
 import { Link } from "react-router-dom";
 import { AboutPageAsync } from "./pages/AboutPage/AboutPage.async";
 import { MainPageAsync } from "./pages/MainPage/MainPage.async";
-import { Suspense } from "react";
+import { Suspense, useContext, useState } from "react";
+import { Theme, ThemeContext } from "./theme/ThemeContext";
+import { useTheme } from "./theme/useTheme";
 
 export const App = () => {
+  const { theme, toggleTheme } = useTheme();
+
   return (
-    <div className="app">
+    <div className={`app ${theme}`}>
       <Link to={"/"}>Главная</Link>
       <Link to={"/about"}>О сайте</Link>
+      <button onClick={toggleTheme}>Toggle theme</button>
       <Suspense fallback={<div>Loading...</div>}>
         <Routes>
           <Route path={"/about"} element={<AboutPageAsync />} />

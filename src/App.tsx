4abaf11c60581437eaf6837199f6ed27dcb35d04
Тавабилов Денис Ринатович <d.tavabilov@prosoftsystems.ru>
2023-06-08 08:17:49 +0500

    Добавление хелпера стилей 1.8

--- src/App.tsx
+++ src/App.tsx
@@ -6,12 +6,13 @@ import { MainPageAsync } from "./pages/MainPage/MainPage.async";
 import { Suspense, useContext, useState } from "react";
 import { Theme, ThemeContext } from "./theme/ThemeContext";
 import { useTheme } from "./theme/useTheme";
+import { classNames } from "./helpers/classNames/classNames";
 
 export const App = () => {
   const { theme, toggleTheme } = useTheme();
 
   return (
-    <div className={`app ${theme}`}>
+    <div className={classNames("app", {}, [theme])}>
       <Link to={"/"}>Главная</Link>
       <Link to={"/about"}>О сайте</Link>
       <button onClick={toggleTheme}>Toggle theme</button>

c00cb16a59d831ef77c2866862f5b4497b29fc5e
Денис Тавабилов <dagger1233@gmail.com>
2023-06-16 09:22:41 +0500

    Свг лоадеры, файл лоадеры, баттон 13 урок

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -2,7 +2,21 @@ import webpack from "webpack";
 import MiniCssExtractPlugin from "mini-css-extract-plugin";
 import { BuildOptions } from "./types/config";
 
-export function buildLoaders({isDev}: BuildOptions): webpack.RuleSetRule[] {
+export function buildLoaders({ isDev }: BuildOptions): webpack.RuleSetRule[] {
+  const svgLoader = {
+    test: /\.svg$/,
+    use: ["@svgr/webpack"],
+  };
+
+  const fileLoader = {
+    test: /\.(png|jpe?g|gif|woff2|woff)$/i,
+    use: [
+      {
+        loader: "file-loader",
+      },
+    ],
+  };
+
   const scssLoader = {
     test: /\.s[ac]ss$/i,
     use: [
@@ -13,12 +27,12 @@ export function buildLoaders({isDev}: BuildOptions): webpack.RuleSetRule[] {
         loader: "css-loader",
         options: {
           modules: {
-            auto: (resPath:string) => Boolean(resPath.includes('.module.')),
-            localIdentName: isDev 
-            ? '[path][name]__[local]--[hash:base64:5]'
-            : '[hash:base64:8]'
-          }
-        }
+            auto: (resPath: string) => Boolean(resPath.includes(".module.")),
+            localIdentName: isDev
+              ? "[path][name]__[local]--[hash:base64:5]"
+              : "[hash:base64:8]",
+          },
+        },
       },
       // Compiles Sass to CSS
       "sass-loader",
@@ -29,5 +43,5 @@ export function buildLoaders({isDev}: BuildOptions): webpack.RuleSetRule[] {
     use: "ts-loader",
     exclude: /node_modules/,
   };
-  return [typescriptLoader, scssLoader];
+  return [typescriptLoader, scssLoader, svgLoader, fileLoader];
 }
--- src/app/App.tsx
+++ src/app/App.tsx
@@ -9,11 +9,10 @@ import { AppRouter } from "./providers/router";
 import { Navbar } from "widgets/Navbar";
 
 export const App = () => {
-  const { theme, toggleTheme } = useTheme();
+  const { theme } = useTheme();
 
   return (
     <div className={classNames("app", {}, [theme])}>
-      <button onClick={toggleTheme}>Toggle theme</button>
       <Navbar />
       <AppRouter />
     </div>
